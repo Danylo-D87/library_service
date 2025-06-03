2ef0edffe322a209d095cddd0f217e05b0983bfe
Danylo-D87 <quaestor88@gmail.com>
2025-06-03 16:43:54 +0300

    hide secret stripe secret key

--- config/settings.py
+++ config/settings.py
@@ -148,8 +148,8 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": False,
 }
 
-STRIPE_SECRET_KEY = "sk_test_51RVr2GP2o4kbBVlJ1yOXQdhvtVjsKsdWqerZxEMjK4sJsqlp0RECB51AFx6sf9W3SliiIfRblvKcnnfHxfZW95Zb00XCva6RSZ"
-STRIPE_PUBLISHABLE_KEY = "pk_test_51RVr2GP2o4kbBVlJ5rLZb02VVQhw4bvpvGD6sIX9oqRXYbQUDhDMeLcBsvJxVkg7sGj5Sla8gwK9m8FQkpmIjveL00mBgVC4NU"
+STRIPE_SECRET_KEY = ""
+STRIPE_PUBLISHABLE_KEY = ""
 
 STRIPE_SUCCESS_URL = "https://your-domain.com/payment-success"
 STRIPE_CANCEL_URL = "https://your-domain.com/payment-cancel"
