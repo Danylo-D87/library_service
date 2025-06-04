e5a5b959098350e3c86c6634db73d8c03ae77969
Danylo-D87 <quaestor88@gmail.com>
2025-06-04 12:50:33 +0300

    add docker config

--- config/settings.py
+++ config/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        # "HOST": "localhost",
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
@@ -157,3 +162,6 @@ STRIPE_PUBLISHABLE_KEY = os.getenv("STRIPE_PUBLISHABLE_KEY")
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+broker_connection_retry_on_startup = True
